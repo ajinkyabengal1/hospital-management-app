27b87763af8d0a5ca9cca543dbf763f5f7314d9d
ajinkyabengal1 <ajinkyabengal4871@gmail.com>
2024-05-12 02:43:27 +0530

    latest changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import heroImg1 from "../assets/images/doctor-img01.png";
 import heroImg2 from "../assets/images/doctor-img02.png";
 import heroImg3 from "../assets/images/doctor-img03.png";
+import faqImg from "../assets/images/faq-img.png";
 import icon1 from "../assets/images/icon01.png";
 import icon2 from "../assets/images/icon02.png";
 import icon3 from "../assets/images/icon03.png";
+import featureImg from "../assets/images/feature-img.png";
+import videoImg from "../assets/images/video-icon.png";
+import avatarIcon from "../assets/images/avatar-icon.png";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import About from "../components/About/About";
+import ServiceList from "../components/Services/ServiceList";
+import DoctorsList from "../components/Doctors/DoctorsList";
+import FaqList from "../components/Faq/FaqList";
 
 const Home = () => {
   return (
@@ -81,6 +89,7 @@ const Home = () => {
       </section>
       {/* hero section end*/}
 
+      {/* cards section */}
       <section>
         <div className="container">
           <div className="lg:w-[470px] mx-auto">
@@ -168,6 +177,122 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      {/* about section */}
+      <About />
+
+      {/* services section start*/}
+      <section>
+        {" "}
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our medical services</h2>
+            <p className="text_para text-center">
+              World class care for everyone our health system offers unmatched,
+              expert health care.
+            </p>
+          </div>
+          <ServiceList />
+        </div>
+      </section>
+      {/* services section end*/}
+
+      {/* feature section start */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* <=== feature content ===> */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get virtual treatment <br /> anytime.
+              </h2>
+              <ul className="pl-4">
+                <li className="text_para">
+                  1. Schedule the appointment directly.
+                </li>
+                <li className="text_para">
+                  2. Search for your physician here, contact their office .
+                </li>
+                <li className="text_para">
+                  3. View your physician who are accepting new patients, use the
+                  online scheduling tool to select an appointment time.
+                </li>
+              </ul>
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
+
+            {/* <=== feature img ===> */}
+            <div className="relative z-10 xl:w-[770px]  flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} className="w-3/4" alt="Loading" />
+
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue, 11
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[400]">
+                      10:00 AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                    <img src={videoImg} alt="Loading" />
+                  </span>
+                </div>
+
+                <div className="w-[65px] rounded-full lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 ">
+                  Consultation
+                </div>
+
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="Loading" />
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                    Jignes Patel
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* feature section end */}
+
+      {/* Our Doctors card start */}
+      <section>
+        <div className="container">
+          <div className="lg:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our great doctors</h2>
+            <p className="text_para text-center">
+              World-class care for everyone. Our health system offers unmatched,
+              expert health care.
+            </p>
+          </div>
+          {/* doc List */}
+          <DoctorsList />
+        </div>
+      </section>
+      {/* Our Doctors card end */}
+
+      {/* faq section start */}
+      <section>
+        <div className="container">
+          <div className="flex items-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="loading" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* faq section end */}
     </>
   );
 };
